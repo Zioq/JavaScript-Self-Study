387e413f277c7e91083881111bb1e4227581947a
Zioq <jr.roberthan@gmail.com>
2020-09-20 23:23:55 -0700

    update API

---
+++ 7.HTTP Requests/Mini_project/main.js
@@ -0,0 +1,9 @@
+// Set the Foursquare API 
+const clientId = 'HAADIKSZIZXTBTEMDQPEY42OLRME2UPO23OM5LR5Y3U2Q4QY';
+const clinetSecret = 'Z4VNFUULXBP0R1L52JYD0KECM1K5JC1Y3AWKIVOHINCA4QVK';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
+
+// Set the OpenWeather API
+const openWeatherKey = 'fd3ee3aacf7b96f369c35f9b11b46187';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
